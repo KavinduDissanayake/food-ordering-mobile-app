fad72e2b465bb96d9d803f2d9dea4d4d4309b596
pankaja <pankajaw.emedia@gmail.com>
2022-09-09 21:26:35 +0530

    notification screen added

---
+++ lib/screens/notificationScreen.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:foodapp/const/colors.dart';
+import 'package:foodapp/utils/helper.dart';
+
+class NotificationScreen extends StatelessWidget {
+  const NotificationScreen({Key? key}) : super(key: key);
+  static const routeName = "/notiScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SafeArea(
+              child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back_ios_rounded,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "Notifications",
+                        style: Helper.getTheme(context).headline5,
+                      ),
+                    ),
+                    Image.asset(
+                      Helper.getAssetName("cart.png", "virtual"),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const NotiCard(
+                title: "Your order has been picked up",
+                time: "Now",
+              ),
+              const NotiCard(
+                title: "Your order has been delivered",
+                time: "1 h ago",
+                color: AppColor.placeholderBg,
+              ),
+              const NotiCard(
+                title: "Lorem ipsum dolor sit amet, consectetur",
+                time: "3 h ago",
+              ),
+              const NotiCard(
+                title: "Lorem ipsum dolor sit amet, consectetur",
+                time: "5 h ago",
+              ),
+              const NotiCard(
+                title: "Lorem ipsum dolor sit amet, consectetur",
+                time: "05 Sep 2020",
+                color: AppColor.placeholderBg,
+              ),
+              const NotiCard(
+                title: "Lorem ipsum dolor sit amet, consectetur",
+                time: "12 Aug 2020",
+                color: AppColor.placeholderBg,
+              ),
+              const NotiCard(
+                title: "Lorem ipsum dolor sit amet, consectetur",
+                time: "20 Jul 2020",
+              ),
+              const NotiCard(
+                title: "Lorem ipsum dolor sit amet, consectetur",
+                time: "12 Jul 2020",
+              ),
+            ],
+          )),
+        ],
+      ),
+    );
+  }
+}
+
+class NotiCard extends StatelessWidget {
+  const NotiCard({
+    Key? key,
+    String? time,
+    String? title,
+    Color color = Colors.white,
+  })  : _time = time ?? "",
+        _title = title ?? "",
+        _color = color,
+        super(key: key);
+
+  final String _time;
+  final String _title;
+  final Color _color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: _color,
+        border: const Border(
+          bottom: BorderSide(
+            color: AppColor.placeholder,
+            width: 0.5,
+          ),
+        ),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const CircleAvatar(
+            backgroundColor: AppColor.orange,
+            radius: 5,
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                _title,
+                style: const TextStyle(
+                  color: AppColor.primary,
+                ),
+              ),
+              Text(_time),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
